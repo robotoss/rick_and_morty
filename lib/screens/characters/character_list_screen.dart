39b8f2f4bda872f3e019578f6129d431aded5b43
FreeOn <zoxo@outlook.com>
2021-04-26 23:59:12 +0600

    add subtitle

--- lib/screens/characters/character_list_screen.dart
+++ lib/screens/characters/character_list_screen.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:rick_and_morty/components/text_filds/app_bar_search_text_field.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:rick_and_morty/theme/app_text_styles.dart';
+import 'package:rick_and_morty/theme/rick_morty_icons.dart';
 
 class CharacterListScreen extends StatelessWidget {
   const CharacterListScreen({Key? key}) : super(key: key);
@@ -11,10 +13,37 @@ class CharacterListScreen extends StatelessWidget {
       body: CustomScrollView(
         slivers: [
           SliverAppBar(
+            floating: true,
             title: Padding(
               padding: const EdgeInsets.only(top: 10),
-              child: AppBarSearchTextField(
-                hintText: AppLocalizations.of(context)!.find_a_character,
+              child: Column(
+                children: [
+                  AppBarSearchTextField(
+                    hintText: AppLocalizations.of(context)!.find_a_character,
+                  ),
+                ],
+              ),
+            ),
+            bottom: PreferredSize(
+              preferredSize: Size.fromHeight(60.0),
+              child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 16),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      AppLocalizations.of(context)!.total_characters(100),
+                      style: AppTextStyles.subTitle,
+                    ),
+                    IconButton(
+                      onPressed: () {},
+                      icon: Icon(
+                        RickMorty.grid,
+                        color: Theme.of(context).textTheme.overline!.color,
+                      ),
+                    )
+                  ],
+                ),
               ),
             ),
           ),
