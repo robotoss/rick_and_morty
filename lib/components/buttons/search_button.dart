eec554391251a09f604e038f7c9647f6223accaf
FreeOn <zoxo@outlook.com>
2021-05-29 00:07:39 +0600

    fix search screen

--- lib/components/buttons/search_button.dart
+++ lib/components/buttons/search_button.dart
@@ -12,67 +12,64 @@ class SearchFilterButton extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.only(top: 10),
-      child: Row(
-        children: [
-          Expanded(
-            child: GestureDetector(
-              onTap: () => onTap,
-              child: Container(
-                height: 56,
-                decoration: BoxDecoration(
-                  color: Theme.of(context).dividerColor,
-                  borderRadius: BorderRadius.horizontal(
-                    left: Radius.circular(100),
-                  ),
-                ),
-                child: Row(
-                  children: [
-                    const SizedBox(width: 15),
-                    Icon(
-                      RickMorty.search,
-                      color: Theme.of(context).textTheme.overline!.color,
-                    ),
-                    const SizedBox(width: 10),
-                    Text(
-                      title,
-                      style: Theme.of(context).textTheme.bodyText1,
-                    ),
-                  ],
-                ),
-              ),
-            ),
-          ),
-          GestureDetector(
-            onTap: () {},
+    return Row(
+      children: [
+        Expanded(
+          child: GestureDetector(
+            onTap: () => onTap,
             child: Container(
               height: 56,
-              width: 50,
               decoration: BoxDecoration(
                 color: Theme.of(context).dividerColor,
                 borderRadius: BorderRadius.horizontal(
-                  right: Radius.circular(100),
+                  left: Radius.circular(100),
                 ),
               ),
               child: Row(
                 children: [
-                  Container(
-                    width: 1,
-                    height: 24,
+                  const SizedBox(width: 15),
+                  Icon(
+                    RickMorty.search,
                     color: Theme.of(context).textTheme.overline!.color,
                   ),
                   const SizedBox(width: 10),
-                  Icon(
-                    RickMorty.filter,
-                    color: Theme.of(context).textTheme.overline!.color,
+                  Text(
+                    title,
+                    style: Theme.of(context).textTheme.bodyText1,
                   ),
                 ],
               ),
             ),
-          )
-        ],
-      ),
+          ),
+        ),
+        GestureDetector(
+          onTap: () {},
+          child: Container(
+            height: 56,
+            width: 50,
+            decoration: BoxDecoration(
+              color: Theme.of(context).dividerColor,
+              borderRadius: BorderRadius.horizontal(
+                right: Radius.circular(100),
+              ),
+            ),
+            child: Row(
+              children: [
+                Container(
+                  width: 1,
+                  height: 24,
+                  color: Theme.of(context).textTheme.overline!.color,
+                ),
+                const SizedBox(width: 10),
+                Icon(
+                  RickMorty.filter,
+                  color: Theme.of(context).textTheme.overline!.color,
+                ),
+              ],
+            ),
+          ),
+        )
+      ],
     );
   }
 }
